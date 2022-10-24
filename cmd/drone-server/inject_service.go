5dcd0586d969f4b7906edc148492d5d51247c1ad
jianguoyong <jianguoyong@dm-ai.cn>
2022-10-24 15:25:39 +0800

    替换drone的git仓库url 2

---
+++ cmd/drone-server/inject_service.go
@@ -0,0 +1,229 @@
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"time"
+
+	"github.com/mailbyms/drone/cmd/drone-server/config"
+	"github.com/mailbyms/drone/core"
+	"github.com/mailbyms/drone/livelog"
+	"github.com/mailbyms/drone/metric/sink"
+	"github.com/mailbyms/drone/pubsub"
+	"github.com/mailbyms/drone/service/canceler"
+	"github.com/mailbyms/drone/service/canceler/reaper"
+	"github.com/mailbyms/drone/service/commit"
+	contents "github.com/mailbyms/drone/service/content"
+	"github.com/mailbyms/drone/service/content/cache"
+	"github.com/mailbyms/drone/service/hook"
+	"github.com/mailbyms/drone/service/hook/parser"
+	"github.com/mailbyms/drone/service/linker"
+	"github.com/mailbyms/drone/service/netrc"
+	orgs "github.com/mailbyms/drone/service/org"
+	"github.com/mailbyms/drone/service/repo"
+	"github.com/mailbyms/drone/service/status"
+	"github.com/mailbyms/drone/service/syncer"
+	"github.com/mailbyms/drone/service/token"
+	"github.com/mailbyms/drone/service/transfer"
+	"github.com/mailbyms/drone/service/user"
+	"github.com/mailbyms/drone/session"
+	"github.com/mailbyms/drone/trigger"
+	"github.com/mailbyms/drone/trigger/cron"
+	"github.com/mailbyms/drone/version"
+	"github.com/mailbyms/go-scm/scm"
+
+	"github.com/google/wire"
+)
+
+// wire set for loading the services.
+var serviceSet = wire.NewSet(
+	canceler.New,
+	commit.New,
+	cron.New,
+	livelog.New,
+	linker.New,
+	parser.New,
+	pubsub.New,
+	token.Renewer,
+	transfer.New,
+	trigger.New,
+	user.New,
+
+	provideRepositoryService,
+	provideContentService,
+	provideDatadog,
+	provideHookService,
+	provideNetrcService,
+	provideOrgService,
+	provideReaper,
+	provideSession,
+	provideStatusService,
+	provideSyncer,
+	provideSystem,
+)
+
+// provideContentService is a Wire provider function that
+// returns a contents service wrapped with a simple LRU cache.
+func provideContentService(client *scm.Client, renewer core.Renewer) core.FileService {
+	return cache.Contents(
+		contents.New(client, renewer),
+	)
+}
+
+// provideHookService is a Wire provider function that returns a
+// hook service based on the environment configuration.
+func provideHookService(client *scm.Client, renewer core.Renewer, config config.Config) core.HookService {
+	return hook.New(client, config.Proxy.Addr, renewer)
+}
+
+// provideNetrcService is a Wire provider function that returns
+// a netrc service based on the environment configuration.
+func provideNetrcService(client *scm.Client, renewer core.Renewer, config config.Config) core.NetrcService {
+	return netrc.New(
+		client,
+		renewer,
+		config.Cloning.AlwaysAuth,
+		config.Cloning.Username,
+		config.Cloning.Password,
+	)
+}
+
+// provideOrgService is a Wire provider function that
+// returns an organization service wrapped with a simple cache.
+func provideOrgService(client *scm.Client, renewer core.Renewer) core.OrganizationService {
+	return orgs.NewCache(orgs.New(client, renewer), 10, time.Minute*5)
+}
+
+// provideRepo is a Wire provider function that returns
+// a repo based on the environment configuration
+func provideRepositoryService(client *scm.Client, renewer core.Renewer, config config.Config) core.RepositoryService {
+	return repo.New(
+		client,
+		renewer,
+		config.Repository.Visibility,
+		config.Repository.Trusted,
+	)
+}
+
+// provideSession is a Wire provider function that returns a
+// user session based on the environment configuration.
+func provideSession(store core.UserStore, config config.Config) (core.Session, error) {
+	if config.Session.MappingFile != "" {
+		return session.Legacy(store, session.Config{
+			Secure:      config.Session.Secure,
+			Secret:      config.Session.Secret,
+			Timeout:     config.Session.Timeout,
+			MappingFile: config.Session.MappingFile,
+		})
+	}
+
+	return session.New(store, session.NewConfig(
+		config.Session.Secret,
+		config.Session.Timeout,
+		config.Session.Secure),
+	), nil
+}
+
+// provideUserService is a Wire provider function that returns a
+// user service based on the environment configuration.
+func provideStatusService(client *scm.Client, renewer core.Renewer, config config.Config) core.StatusService {
+	return status.New(client, renewer, status.Config{
+		Base:     config.Server.Addr,
+		Name:     config.Status.Name,
+		Disabled: config.Status.Disabled,
+	})
+}
+
+// provideSyncer is a Wire provider function that returns a
+// repository synchronizer.
+func provideSyncer(repoz core.RepositoryService,
+	repos core.RepositoryStore,
+	users core.UserStore,
+	batch core.Batcher,
+	config config.Config) core.Syncer {
+	sync := syncer.New(repoz, repos, users, batch)
+	// the user can define a filter that limits which
+	// repositories can be synchronized and stored in the
+	// database.
+	if filter := config.Repository.Filter; len(filter) > 0 {
+		sync.SetFilter(syncer.NamespaceFilter(filter))
+	}
+	return sync
+}
+
+// provideSyncer is a Wire provider function that returns the
+// system details structure.
+func provideSystem(config config.Config) *core.System {
+	return &core.System{
+		Proto:   config.Server.Proto,
+		Host:    config.Server.Host,
+		Link:    config.Server.Addr,
+		Version: version.Version.String(),
+	}
+}
+
+// provideReaper is a Wire provider function that returns the
+// zombie build reaper.
+func provideReaper(
+	repos core.RepositoryStore,
+	builds core.BuildStore,
+	stages core.StageStore,
+	canceler core.Canceler,
+	config config.Config,
+) *reaper.Reaper {
+	return reaper.New(
+		repos,
+		builds,
+		stages,
+		canceler,
+		config.Cleanup.Running,
+		config.Cleanup.Pending,
+	)
+}
+
+// provideDatadog is a Wire provider function that returns the
+// datadog sink.
+func provideDatadog(
+	users core.UserStore,
+	repos core.RepositoryStore,
+	builds core.BuildStore,
+	system *core.System,
+	license *core.License,
+	config config.Config,
+) *sink.Datadog {
+	return sink.New(
+		users,
+		repos,
+		builds,
+		*system,
+		sink.Config{
+			Endpoint:         config.Datadog.Endpoint,
+			Token:            config.Datadog.Token,
+			License:          license.Kind,
+			Licensor:         license.Licensor,
+			Subscription:     license.Subscription,
+			EnableGithub:     config.IsGitHub(),
+			EnableGithubEnt:  config.IsGitHubEnterprise(),
+			EnableGitlab:     config.IsGitLab(),
+			EnableBitbucket:  config.IsBitbucket(),
+			EnableStash:      config.IsStash(),
+			EnableGogs:       config.IsGogs(),
+			EnableGitea:      config.IsGitea(),
+			EnableAgents:     !config.Agent.Disabled,
+			EnableNomad:      config.Nomad.Enabled,
+			EnableKubernetes: config.Kube.Enabled,
+		},
+	)
+}
---
+++ handler/api/user/remote/repos.go
@@ -0,0 +1,41 @@
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package remote
+
+import (
+	"net/http"
+
+	"github.com/mailbyms/drone/core"
+	"github.com/mailbyms/drone/handler/api/render"
+	"github.com/mailbyms/drone/handler/api/request"
+	"github.com/mailbyms/drone/logger"
+)
+
+// HandleRepos returns an http.HandlerFunc that write a json-encoded
+// list of repositories to the response body.
+func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		viewer, _ := request.UserFrom(r.Context())
+
+		list, err := repos.List(r.Context(), viewer)
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot list remote repositories")
+		} else {
+			render.JSON(w, list, 200)
+		}
+	}
+}
